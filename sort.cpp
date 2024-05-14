00ce6e9363484282a075ad5223cb95e7678d9b6d
tudaddy <91655254+tudaddy@users.noreply.github.com>
2024-05-14 22:37:01 +0700

    Add files via upload

--- sort.cpp
+++ sort.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+﻿#include <iostream>
 #include "sort.h"
+#include <vector>
 
 void swap(int& a, int& b)
 {
@@ -40,5 +41,101 @@ void SelectionSort(int* arr, int n)
 			if (arr[j] < arr[min_index]) min_index = j;
 		}
 		swap(arr[min_index], arr[i]);
+	}
+}
+
+
+int partition(int arr[], int low, int high)
+{
+	int pivot = arr[high];
+	int i = (low - 1);
+	for (int j = low; j <= high - 1; j++)
+	{
+		if (arr[j] < pivot)
+		{
+			i++;
+			swap(arr[i], arr[j]);
+		}
+	}
+	swap(arr[i + 1], arr[high]);
+	return (i + 1);
+}
+
+void QuickSort1(int arr[], int low, int high)
+{
+	if (low < high)
+	{
+		int pi = partition(arr, low, high);
+
+		QuickSort1(arr, low, pi - 1);
+		QuickSort1(arr, pi + 1, high);
+	}
+}
+
+void merge(int arr[], int l, int m, int r)
+{
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	/* Tạo các mảng tạm */
+	std::vector<int> L, R;
+	L.resize(n1);
+	R.resize(n2);
+
+	/* Copy dữ liệu sang các mảng tạm */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	/* Gộp hai mảng tạm vừa rồi vào mảng arr*/
+	i = 0; // Khởi tạo chỉ số bắt đầu của mảng con đầu tiên
+	j = 0; // Khởi tạo chỉ số bắt đầu của mảng con thứ hai
+	k = l; // IKhởi tạo chỉ số bắt đầu của mảng lưu kết quả
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy các phần tử còn lại của mảng L vào arr nếu có */
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* Copy các phần tử còn lại của mảng R vào arr nếu có */
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+	if (l < r)
+	{
+		// Tương tự (l+r)/2, nhưng cách này tránh tràn số khi l và r lớn
+		int m = l + (r - l) / 2;
+
+		// Gọi hàm đệ quy tiếp tục chia đôi từng nửa mảng
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		merge(arr, l, m, r);
 	}
 }
